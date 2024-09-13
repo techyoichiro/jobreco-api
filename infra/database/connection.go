c7aaf6e41119786f4b4c00f10e5b1ce753d890e9
TechYoichiro <y.119.wonder@gmail.com>
2024-09-13 19:39:38 +0900

    fix :sparkles: #004 CI/CD構築

--- infra/database/connection.go
+++ infra/database/connection.go
@@ -3,6 +3,8 @@ package database
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -14,10 +16,19 @@ func ConnectionDB() (*gorm.DB, error) {
 		return nil, fmt.Errorf("DATABASE_URL environment variable is not set")
 	}
 
+	// TimeZoneパラメータを追加
+	if !strings.Contains(databaseURL, "TimeZone=") {
+		databaseURL += "&TimeZone=Asia/Tokyo"
+	}
+
 	db, err := gorm.Open(postgres.New(postgres.Config{
 		DSN:                  databaseURL,
 		PreferSimpleProtocol: true,
-	}), &gorm.Config{})
+	}), &gorm.Config{
+		NowFunc: func() time.Time {
+			return time.Now().In(time.FixedZone("Asia/Tokyo", 9*60*60))
+		},
+	})
 
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
