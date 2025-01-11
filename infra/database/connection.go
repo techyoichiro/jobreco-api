f124ec44817223e867ef8045a929d44a9e08829a
TechYoichiro <y.119.wonder@gmail.com>
2025-01-11 14:55:32 +0900

    fix 日付を跨いで退勤をできるように修正

--- infra/database/connection.go
+++ infra/database/connection.go
@@ -3,33 +3,13 @@ package database
 import (
 	"fmt"
 	"os"
-	"strings"
-	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func ConnectionDB() (*gorm.DB, error) {
-	databaseURL := os.Getenv("DATABASE_URL")
-	if databaseURL == "" {
-		return nil, fmt.Errorf("DATABASE_URL environment variable is not set")
-	}
-
-	// TimeZoneパラメータを追加
-	if !strings.Contains(databaseURL, "TimeZone=") {
-		databaseURL += "&TimeZone=Asia/Tokyo"
-	}
-
-	db, err := gorm.Open(postgres.New(postgres.Config{
-		DSN:                  databaseURL,
-		PreferSimpleProtocol: true,
-	}), &gorm.Config{
-		NowFunc: func() time.Time {
-			return time.Now().In(time.FixedZone("Asia/Tokyo", 9*60*60))
-		},
-	})
-
+	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
